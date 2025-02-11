09c02f063590766c03c3d87f8c931ee9e8fbe2cd
PawelKTech <pawel.kozanecki.06@gmail.com>
2025-02-12 00:42:12 +0100

    Improving desgin and starting js

--- script/script.js
+++ script/script.js
@@ -1,21 +1,13 @@
-let options = document.querySelectorAll('.option');
-let divcontent = document.querySelector('.content');
-options.forEach(element => {
-    element.addEventListener('click', () => {
-        console.log(element.innerHTML);
-        divcontent.innerHTML =`
-        <input type="text" id="input1">
-        <input type="text" id="input2">
-        <button>Pobierz wartości</button>
-    `;
+let options = document.querySelectorAll(".option");
+let result = document.querySelector(".result");
+let lengthinput = document.querySelector("#length");
+let convertFromInput = document.querySelector("#convertFromInput");
+let convertToInput = document.querySelector("#convertToInput");
+let choosenUnit = "length";
 
-    document.querySelector("button").addEventListener("click", () => {
-        const input1Value = document.getElementById("input1").value;
-        const input2Value = document.getElementById("input2").value;
-        console.log("Wartość pierwszego inputa:", input1Value);
-        console.log("Wartość drugiego inputa:", input2Value);
-    });
-    });
+options.forEach((element) => {
+  element.addEventListener("click", () => {
+    choosenUnit = element.innerHTML;
+    console.log(choosenUnit);
+  });
 });
-console.log(options)
-console.log(options[1])
