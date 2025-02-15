c5ea1bb1ad730d9a0a15700e2a8a524689b74520
Pawel <pawel.kozanecki.06@gmail.com>
2025-02-15 20:01:40 +0100

    Added chganging selecet based on navbar choose

--- script/script.js
+++ script/script.js
@@ -1,13 +1,41 @@
-let options = document.querySelectorAll(".option");
-let result = document.querySelector(".result");
-let lengthinput = document.querySelector("#length");
-let convertFromInput = document.querySelector("#convertFromInput");
-let convertToInput = document.querySelector("#convertToInput");
-let choosenUnit = "length";
+const calculateButton = document.querySelector("#calculate-button");
+const navbar = document.querySelector(".navbar");
+const fromSelect = document.querySelector("#convertFromInput");
+const toSelect = document.querySelector("#convertToInput");
 
-options.forEach((element) => {
-  element.addEventListener("click", () => {
-    choosenUnit = element.innerHTML;
-    console.log(choosenUnit);
-  });
+let chosenNavbarOption = "Length";
+
+calculateButton.addEventListener("click", () => {
+  const numberInput = document.querySelector("#number-input").value;
+  const selectedFrom = fromSelect.value;
+  const selectedTo = toSelect.value;
+
+  console.log(`${numberInput} ${selectedFrom} ${selectedTo}`);
 });
+
+navbar.addEventListener("click", (event) => {
+  if (event.target.classList.contains("navbar_option")) {
+    chosenNavbarOption = event.target.innerText;
+
+    toggleVisibility(chosenNavbarOption.toLowerCase());
+    updateSelects(chosenNavbarOption.toLowerCase());
+  }
+});
+
+function toggleVisibility(option) {
+  const categories = ["temperature", "length", "weight"];
+
+  categories.forEach((category) => {
+    document.querySelectorAll(`.${category}`).forEach((el) => {
+      el.style.display = category === option ? "block" : "none";
+    });
+  });
+}
+
+function updateSelects(category) {
+  const firstOptionFrom = fromSelect.querySelector(`.${category}`);
+  const firstOptionTo = toSelect.querySelector(`.${category}`);
+
+  if (firstOptionFrom) fromSelect.value = firstOptionFrom.value;
+  if (firstOptionTo) toSelect.value = firstOptionTo.value;
+}
